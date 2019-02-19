5c3ec6840ffe42b0198269701a7559eaa15fbe1f
vvpvvp <alias8516@sina.com.cn>
2019-02-19 18:35:56 +0800

    feat: init files

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ var CONFIG = {
   // 网页 description
   description: 'HeyUI Admin Documents',
   // 页面右上角的 github 链接
-  github: 'https://github.com/heyui/heyui-admin-docs',
+  github: 'https://github.com/heyui/heyui-admin',
   // 左侧目录的md文件
   summaryMd: 'SUMMARY.md',
   // 网页默认访问的md文件
